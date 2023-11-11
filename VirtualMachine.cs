4900514625f10dcd4dca7f69aac7acb8c94075ba
Inv1ctus7 <77233695+Inv1ctus7@users.noreply.github.com>
2023-11-11 12:39:46 +0500

    Update VirtualMachine.cs

--- VirtualMachine.cs
+++ VirtualMachine.cs
@@ -4,24 +4,36 @@ using System.Collections.Generic;
 namespace func.brainfuck
 {
 	public class VirtualMachine : IVirtualMachine
-	{
-		public string Instructions { get; }
-		public int InstructionPointer { get; set; }
-		public byte[] Memory { get; }
-		public int MemoryPointer { get; set; }
+{
+    public string Instructions { get; }
+    public int InstructionPointer { get; set; }
+    public byte[] Memory { get; }
+    public int MemoryPointer { get; set; }
+    private Dictionary<char, Action<IVirtualMachine>> commands;
 
-		public VirtualMachine(string program, int memorySize)
-		{
-		}
+    public VirtualMachine(string program, int memorySize = 30000)
+    {
+        Instructions = program;
+        Memory = new byte[memorySize];
+        commands = new Dictionary<char, Action<IVirtualMachine>>();
+    }
 
-		public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
-		{
-			throw new NotImplementedException();
-		}
+    public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
+    {
+        commands[symbol] = execute;
+    }
 
-		public void Run()
-		{
-			throw new NotImplementedException();
-		}
-	}
+    public void Run()
+    {
+        while (InstructionPointer < Instructions.Length)
+        {
+            char instruction = Instructions[InstructionPointer];
+            if (commands.ContainsKey(instruction))
+            {
+                commands[instruction](this);
+            }
+            InstructionPointer++;
+        }
+    }
+}
 }
